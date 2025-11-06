f937e1652e416dce885fe1b05c725ab3f351ce21
Hassan Akkari <hassan.akkari01@gmail.com>
2025-11-06 14:54:27 +0100

    fix: update base path in Vite config for consistent routing

--- apps/web-react/vite.config.ts
+++ apps/web-react/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/postcss";
 
-const isPages = !!process.env.GITHUB_PAGES;
+// const isPages = !!process.env.GITHUB_PAGES;
 
 export default defineConfig({
-  base: isPages ? "/laboratoire/react/" : "/",
+  base: "/react/",
   plugins: [react()],
   css: {
     postcss: {
